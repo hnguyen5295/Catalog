632eea0e8e7b78f7765e7b5082090df277b9564d
hnguyen5295 <hnguyen5295@gmail.com>
2023-08-01 21:17:53 +0700

    Add health checks package

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
+using System.Net.Mime;
+using System.Text.Json;
 using Catalog.Repositories;
 using Catalog.Settings;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.Configure<MongoDbSettings>(
-    builder.Configuration.GetSection("CatalogDatabase"));
+var mongoDbSettings = builder.Configuration.GetSection("CatalogDatabase");
+var connectionString = mongoDbSettings.Get<MongoDbSettings>().ConnectionString;
+
+builder.Services.Configure<MongoDbSettings>(mongoDbSettings);
 
 builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
 builder.Services.AddControllers(options =>
@@ -16,6 +21,8 @@ builder.Services.AddControllers(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHealthChecks().AddMongoDb(connectionString, name: "mongodb", timeout: TimeSpan.FromSeconds(3), tags: new[] { "ready" });
 
 var app = builder.Build();
 
@@ -31,5 +38,34 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHealthChecks("/health/ready", new HealthCheckOptions
+{
+  Predicate = (check) => check.Tags.Contains("ready"),
+  ResponseWriter = async (context, report) =>
+  {
+    var result = JsonSerializer.Serialize(
+      new
+      {
+        status = report.Status.ToString(),
+        checks = report.Entries.Select(entry => new
+        {
+          name = entry.Key,
+          status = entry.Value.Status.ToString(),
+          exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+          duration = entry.Value.Duration.ToString()
+        })
+      }
+    );
+
+    context.Response.ContentType = MediaTypeNames.Application.Json;
+    await context.Response.WriteAsync(result);
+  }
+});
+
+app.MapHealthChecks("/health/live", new HealthCheckOptions
+{
+  Predicate = (_) => false
+});
 
 app.Run();
--- Settings/MongoDbSettings.cs
+++ Settings/MongoDbSettings.cs
@@ -2,10 +2,24 @@ namespace Catalog.Settings
 {
   public class MongoDbSettings
   {
-    public string ConnectionString { get; set; } = null!;
+    public string Host { get; set; } = null!;
+
+    public string Port { get; set; } = null!;
 
     public string DatabaseName { get; set; } = null!;
 
     public string CollectionName { get; set; } = null!;
+
+    public string User { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string ConnectionString
+    {
+      get
+      {
+        return $"mongodb://{User}:{Password}@{Host}:{Port}";
+      }
+    }
   }
 }
